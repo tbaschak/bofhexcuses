8a63123ceb497f80c7a23966bf01c7413dadf2f4
Theo <theo@voinetworks.ca>
2013-10-28 23:37:42 -0500

    add more excuses

--- js/excuses.js
+++ js/excuses.js
@@ -6,10 +6,12 @@
     "No, 'Google' is not 'the Internet'.",
     "Sunspots.",
     "CDN cache problem.",
+    "CDN upstream problem.",
     "IX made random changes.",
     "Pissed Off the Sysadmin.",
     "Solar Flares.",
-    "Have you tried rebooting your firewall?"
+    "Have you ever patched your firewall?",
+    "Have you rebooted your firewall?"
   ];
 
   var theQuote = quotes[Math.floor(Math.random() * quotes.length)];
